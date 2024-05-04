ece09c57d52b8106da2590d8727269b7b8535dcc
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-03 22:27:40 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Qual o seu nível de glicemia em mg/dL: ");
+int mgdL = Convert.ToInt32(Console.ReadLine());
+
+if(mgdL<92)
+{
+    Console.WriteLine("reavaliar entre 24 e 28 semanas com TTOG 75g de glicose");
+}
+else if(mgdL >= 92 && mgdL <126)
+{
+    Console.WriteLine("Compatível com Diabetes Gestacional");
+}
+else
+{
+    Console.WriteLine("Diabetes Mellitus na Gravidez");
+}
